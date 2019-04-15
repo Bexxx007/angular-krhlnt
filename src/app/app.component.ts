893c8ed9e1facbf7845cf55952842eda235f071b
Bexxx007 <180103024@stu.sdu.edu.kz>
2019-04-15 16:34:18 +0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = "Hello World, My name is Bexxx007";
+  name = "Hello World, My namehg is Bexxx007";
 }
